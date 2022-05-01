ec99f734c716a94fe5bd9e4312734ac651c8d5db
Kimmo Brunfeldt <1232405+kimmobrunfeldt@users.noreply.github.com>
2022-05-02 00:07:37 +0300

    Add fast randbot

--- src/core/bots/availableBots.ts
+++ src/core/bots/availableBots.ts
@@ -1,3 +1,4 @@
+import * as fastRandom from 'src/core/bots/fastRandom'
 import * as random from 'src/core/bots/random'
 
 /**
@@ -5,6 +6,7 @@ import * as random from 'src/core/bots/random'
  */
 export const availableBots = {
   random,
+  fastRandom,
 }
 
 export type BotId = keyof typeof availableBots
---
+++ src/core/bots/fastRandom.ts
@@ -0,0 +1,93 @@
+import _ from 'lodash'
+import { BotCreateOptions, BotImplementation } from 'src/core/bots/framework'
+import { Client } from 'src/core/client'
+import {
+  assertDefined,
+  BOARD_PUSH_POSITIONS,
+  findConnected,
+  getPieceAt,
+} from 'src/core/server/board'
+import * as t from 'src/gameTypes'
+import { loopUntilSuccess } from 'src/utils/utils'
+
+// This is the same bot as random but makes its decisions quick. Makes for a
+// nice demo of spectating rapid game between bots.
+
+export const name = 'Fast randbot'
+
+/**
+ * Warning! You must use `client` as is, because it's a Proxy object for
+ * reconnect purposes. Don't take any properties out of it, it will break.
+ *
+ * Do not:
+ *
+ *   // Bad
+ *   const { serverRpc } = client
+ *
+ *   // Bad
+ *   const getMyPosition = client.serverRpc.getMyPosition()
+ *
+ * Whatever you need to access within client, always use the full property path:
+ *
+ *   client.serverRpc.getMyPosition()
+ *
+ * @returns Promise so that the bot creation can do something async if needed.
+ */
+export async function create({
+  logger,
+  client,
+}: BotCreateOptions): Promise<BotImplementation> {
+  return {
+    async onMyTurn(getState) {
+      const state = getState()
+      // Loop until success, because the bot doesn't understand the blocked push
+      // position rule
+      await loopUntilSuccess(
+        async () => {
+          await push(state, client)
+        },
+        { maxTries: 5, onError: (err) => logger.error('Unable to push', err) }
+      )
+
+      const stateAfterPush = getState()
+      // Do a move
+      await new Promise((resolve) => setTimeout(resolve, 100)) // "thinking time"
+      await move(stateAfterPush, client)
+    },
+  }
+}
+
+async function push(_gameState: t.ClientGameState, client: Client) {
+  // Turn the extra piece randomly
+  await client.serverRpc.setExtraPieceRotation(
+    assertDefined(_.sample<t.Rotation>([0, 90, 180, 270]))
+  )
+
+  const pushPos = assertDefined(_.sample(BOARD_PUSH_POSITIONS))
+  await client.serverRpc.push(pushPos)
+}
+
+async function move(gameState: t.ClientGameState, client: Client) {
+  if (!gameState.myPosition) {
+    throw new Error('My position not found from state')
+  }
+
+  const currentPos = gameState.myPosition
+  const piece = getPieceAt(
+    gameState.board as unknown as t.FilledBoard,
+    currentPos
+  )
+  const connected = _.shuffle(
+    findConnected(
+      gameState.board as unknown as t.Board,
+      new Set([assertDefined(piece)])
+    )
+  )
+
+  // Prefer another piece
+  const newPos =
+    connected.find(
+      (p) => p.position.x !== currentPos.x && p.position.y !== currentPos.y
+    )?.position ?? currentPos
+  await client.serverRpc.move(newPos)
+}
