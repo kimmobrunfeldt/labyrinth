6a92e3cd789b76abec84465973da88db39c85ac0
Kimmo Brunfeldt <1232405+kimmobrunfeldt@users.noreply.github.com>
2022-05-05 23:34:19 +0300

    Remove fast randbot

--- src/core/bots/availableBots.ts
+++ src/core/bots/availableBots.ts
@@ -1,5 +1,4 @@
 import * as bigbrain from 'src/core/bots/bigbrain'
-import * as fastRandom from 'src/core/bots/fastRandom'
 import * as random from 'src/core/bots/random'
 
 /**
@@ -7,7 +6,6 @@ import * as random from 'src/core/bots/random'
  */
 export const availableBots = {
   random,
-  fastRandom,
   bigbrain,
 }
 
